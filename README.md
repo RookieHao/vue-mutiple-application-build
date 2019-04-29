3f3ebbd1295bfe81905fedb57b47cd6e95e74d6c
RookieHao <chenhao2009_219@126.com>
2019-04-29 13:32:32 +0800

    '完善readme,增加项目基础配置文件'

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ```
   此项目适用于vue-cli3搭建的工程,用于多应用集中管理。
   方便多应用之间可以共享组件和依赖包，同步更新依赖版本。
+  参考[vue-cli3移动端多应用项目模板](https://github.com/RookieHao/vue-multiple-app--template)此项目模板使用
 ```
 
 # project start
--- bin/multiple-app.js
+++ bin/multiple-app.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-
+const path = require('path')
 let program = require('commander')
 program.parse(process.argv)
 const [mode,pageModule] = program.args
-
+process.env.VUE_CLI_CONTEXT = path.resolve(__dirname,'../lib')
 require('../lib')(mode,pageModule)
