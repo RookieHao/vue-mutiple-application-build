9767fcb0cf7549400085a2e8715f0aaa1e99b3c6
RookieHao <chenhao2009_219@126.com>
2019-05-06 10:51:09 +0800

    '增加清除打包文件功能'

--- bin/multiple-app.js
+++ bin/multiple-app.js
@@ -2,6 +2,5 @@
 const path = require('path')
 let program = require('commander')
 program.parse(process.argv)
-const [mode,pageModule] = program.args
 process.env.VUE_CLI_CONTEXT = path.resolve(__dirname,'../lib')
-require('../lib')(mode,pageModule)
+require('../lib')(program.args)
